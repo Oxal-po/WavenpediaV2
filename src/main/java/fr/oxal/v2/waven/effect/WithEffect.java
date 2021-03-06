1564a7693c1e60694b1a1c0ccbfd2572f0088b38
oxal-po <wavenpedia@gmail.com>
2021-03-06 19:08:49 +0100

    changement de nom de la fonction getEffects, et ajour de fonction utilitaire pour cette interface

--- src/main/java/fr/oxal/v2/waven/effect/WithEffect.java
+++ src/main/java/fr/oxal/v2/waven/effect/WithEffect.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 public interface WithEffect extends WithRefEntity {
 
-    default List<WavenEffect> getEffects(){
+    default List<WavenEffect> getWavenEffects() {
         ArrayList<WavenEffect> l = new ArrayList<>();
         getWavenRef().ifPresent(a -> {
-            for (JsonElement e : a){
-                if (e.isJsonPrimitive() && e.getAsJsonPrimitive().isString()){
+            for (JsonElement e : a) {
+                if (e.isJsonPrimitive() && e.getAsJsonPrimitive().isString()) {
                     l.add(new WavenEffect(e.getAsString()));
                 }
             }
@@ -23,12 +23,20 @@ public interface WithEffect extends WithRefEntity {
     default List<String> getNameEffects(){
         ArrayList<String> l = new ArrayList<>();
         getWavenRef().ifPresent(a -> {
-            for (JsonElement e : a){
-                if (e.isJsonPrimitive() && e.getAsJsonPrimitive().isString()){
+            for (JsonElement e : a) {
+                if (e.isJsonPrimitive() && e.getAsJsonPrimitive().isString()) {
                     l.add(e.getAsString());
                 }
             }
         });
         return l;
+    }
+
+    default boolean containKeyEffect(String name) {
+        return getNameEffects().contains(name);
+    }
+
+    default boolean containEffect(WavenEffect effect) {
+        return getWavenEffects().contains(effect);
     }
 }
